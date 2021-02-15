74ca4410db955a58fc5ba735b99267c25855bf56
felipetodev <fe.ossandon.u@gmail.com>
2021-02-14 21:18:35 -0300

    better csv to json converter

--- scripts/transform-csv-to-json.js
+++ scripts/transform-csv-to-json.js
@@ -1,12 +1,7 @@
-const XLSX = require('xlsx')
+const csvtojson = require('csvtojson');
 
-module.exports = async function transformCsvToJson (csvFileName) {
-    const workbook = XLSX.readFile(`./data/${csvFileName}`)
-
-    // Search another parser
-    const { Sheets: { Sheet1 } } = workbook
-    
-    const json = XLSX.utils.sheet_to_json(Sheet1)
+module.exports = async function transformCsvToJson(csvFileName) {
+    const json = await csvtojson().fromFile(`./data/${csvFileName}`)
 
     return json
 }
