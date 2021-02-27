3ca64c0dfd6b4915d75e779d0f4eceb78559f8f5
felipetodev <fe.ossandon.u@gmail.com>
2021-02-27 00:26:56 -0300

    intersection observer 👁

--- components/ProgressChart/index.js
+++ components/ProgressChart/index.js
@@ -1,17 +1,25 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
 import { CustomXTick, CustomYTick } from './ticks'
 import styles from './styles/ProgressChart.module.css'
 import SelectRegion from 'components/SelectRegion'
+import useIntersectionObserver from 'hooks/useIntersectionObserver'
 
 export default function ProgressChart({ tooltip: CustomTooltip, data }) {
     const [region, setRegion] = useState(0)
-    
+
+    const elementRef = useRef(null)
+
+    const [isVisible] = useIntersectionObserver({
+        elementRef,
+        freezeOnceVisible: true
+    })
+
     const chartDatasetPrimera = data[region]
-    const chartDatasetSegunda = data[+region+1]
-    
+    const chartDatasetSegunda = data[+region + 1]
+
     const chartDatasetTotal = []
-    
+
     for (let i = 0; i < chartDatasetPrimera.length || i < chartDatasetSegunda.length; i++) {
         const primeras = chartDatasetPrimera[i] || {};
         const segundas = chartDatasetSegunda[i] || {};
@@ -26,43 +34,45 @@ export default function ProgressChart({ tooltip: CustomTooltip, data }) {
     if (!chartDatasetTotal) return null
 
     return (
-        <div className={styles.chartContainer}>
-            <div style={{ width: '100%', height: 450 }}>
-                <SelectRegion onChange={setRegion}/>
-                <ResponsiveContainer>
-                    <AreaChart
-                        data={chartDatasetTotal}
-                        margin={{
-                            top: 50,
-                            right: 0,
-                            left: 0,
-                            bottom: 50
-                        }}
-                    >
-                        <XAxis dataKey='age' tick={<CustomXTick />} />
-                        <YAxis
-                            domain={[0, 'dataMax']}
-                            interval='preserveStartEnd'
-                            width={100}
-                            scale='linear'
-                            tick={<CustomYTick />}
-                        />
-                        <Tooltip content={<CustomTooltip />} />
-                        <Area
-                            type='monotone'
-                            dataKey='dataPrimeras'
-                            stroke='#001e63'
-                            fill='#d2effd'
-                        />
-                        <Area
-                            type='monotone'
-                            dataKey='dataSegundas'
-                            stroke='#1757E2'
-                            fill='#1757E2'
-                        />
-                    </AreaChart>
-                </ResponsiveContainer>
-            </div>
+        <div className={styles.chartContainer} ref={elementRef}>
+            {isVisible && (
+                <div style={{ width: '100%', height: 450 }}>
+                    <SelectRegion onChange={setRegion} />
+                    <ResponsiveContainer>
+                        <AreaChart
+                            data={chartDatasetTotal}
+                            margin={{
+                                top: 50,
+                                right: 0,
+                                left: 0,
+                                bottom: 50
+                            }}
+                        >
+                            <XAxis dataKey='age' tick={<CustomXTick />} />
+                            <YAxis
+                                domain={[0, 'dataMax']}
+                                interval='preserveStartEnd'
+                                width={100}
+                                scale='linear'
+                                tick={<CustomYTick />}
+                            />
+                            <Tooltip content={<CustomTooltip />} />
+                            <Area
+                                type='monotone'
+                                dataKey='dataPrimeras'
+                                stroke='#001e63'
+                                fill='#d2effd'
+                            />
+                            <Area
+                                type='monotone'
+                                dataKey='dataSegundas'
+                                stroke='#1757E2'
+                                fill='#1757E2'
+                            />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+            )}
         </div>
     )
 }
